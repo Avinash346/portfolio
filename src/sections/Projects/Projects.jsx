9000bd5fe695da27a0ac6e1e348f4f5a2c8c4e3e
Avinash346 <mail2avinashkumar1@gmail.com>
2024-07-03 13:06:28 +0530

    new deploy

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,58 @@
+import styles from './ProjectsStyles.module.css';
+import dhun from '../../assets/dhun.png';
+import company from '../../assets/company.png';
+import HotelPage from '../../assets/HotelPage.png';
+import travell from '../../assets/travell.png';
+import budjet from '../../assets/budjet.png';
+import django from '../../assets/django.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={dhun}
+          link="https://dhun-27yl8hxzm-avinashs-projects-d9cd0e18.vercel.app/"
+          h3="dhun"
+          p="Music streaming App"
+        />
+        <ProjectCard
+          src={company}
+          link="https://celadon-stroopwafel-3ea309.netlify.app/"
+          h3="corporate website"
+          p="A "
+        />
+        
+        <ProjectCard
+          src={travell}
+          link="https://avinash346.github.io/AirTravel-webpage/"
+          h3="Air travell"
+          p="Air travel webpage"
+        />
+        <ProjectCard
+          src={HotelPage}
+          link="https://www.linkedin.com/feed/update/urn:li:activity:7211741628917587971/"
+          h3="Arklyte"
+          p="A travell booking website"
+        />
+        <ProjectCard
+          src={budjet}
+          link="https://avinash346.github.io/budget-tracker/"
+          h3="Budget tracker"
+          p="Expance tracker website"
+        />
+
+        <ProjectCard
+          src={django}
+          link="https://www.linkedin.com/posts/avinash-kumar-745b03239_backenddevelopment-django-djangorestframework-activity-7151606432608473088-obRd?utm_source=share&utm_medium=member_desktop"
+          h3="projectX"
+          p="A CRM website"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,41 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="python" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="Next-js" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="bootstrap" />
+        <SkillList src={checkMarkIcon} skill="material ui" />
+        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Redux" />
+        <SkillList src={checkMarkIcon} skill="Webpack" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Django" />
+        <SkillList src={checkMarkIcon} skill="my sql" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
